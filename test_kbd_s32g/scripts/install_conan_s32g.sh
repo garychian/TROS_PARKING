43db1552a975f199787000dfb0d3141419c33b82
jiamin.yang <jiamin.yang@horizon.cc>
2024-07-10 18:58:15 +0800

    [fix]: Upgrade version of parking to 1.0.1

---
+++ test_kbd_s32g/scripts/install_conan_s32g.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp -r ./convert_assistant/2.5.3/thirdparty/stable/package/d416bd0169399d6afc0598870a0da6ad51927f6b ~/.conan/data/convert_assistant/2.5.3/thirdparty/stable/package
+cp -r ./fmt/9.0.0/thirdparty/stable/package/64e6804379cf473ebf9e8250ea8032584a52abd7 ~/.conan/data/fmt/9.0.0/thirdparty/stable/package
+cp -r ./communication/1.13.5/bole/stable/package/1bd335bd435d2b5419bdb5164c6ddd0e9a3cb232 ~/.conan/data/communication/1.13.5/bole/stable/package
+cp -r ./hlog/1.13.8/bole/stable/package/dfc1b953325444d30ea038da80ec4743ef7fa89f ~/.conan/data/hlog/1.13.8/bole/stable/package
+cp -r ./schedulegroup/1.13.2/bole/stable/package/065eaaf82931843439e0f635485cbf955551d710 ~/.conan/data/schedulegroup/1.13.2/bole/stable/package
